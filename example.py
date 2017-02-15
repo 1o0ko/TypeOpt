4925c4dbda808a73529016672c51af94cadd86ae
1o0ko <wojciech.stokowiec@gmail.com>
2017-02-15 15:08:21 +0100

    simplified example file

--- example.py
+++ example.py
@@ -11,15 +11,10 @@ Options:
     --float-option=<float>      Optionally change value to something other
                                 float [default: 0.2]
 """
-import itertools
-
 from typeopt import Arguments
 
 if __name__ == '__main__':
 
-    args = Arguments(__doc__, version='GloVe 0.1')
+    arguments = Arguments(__doc__, version='example 0.1')
 
-    print(args)
-    with open(args.path) as file_:
-        for index, line in enumerate(itertools.islice(file_, 10)):
-            print("Line {0}: {1}".format(index, line))
+    print(arguments)
