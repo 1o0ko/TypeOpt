85c113945bc13deed0c5bb3aaf2ac380575d52d9
1o0ko <wojciech.stokowiec@gmail.com>
2017-02-20 20:59:07 +0100

    added tarball to setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
 from setuptools import setup
 
 
-setup(name='typeopt',
-      version='0.1',
-      description='Typed docopt',
-      url='https://github.com/nowthisnews/typeopt',
-      author='Wojciech Stokowiec',
-      author_email='wojciech.stokowiec@tooploox.com',
-      packages=['typeopt'],
-      install_requires=[
-          'docopt',
-      ],
-      zip_safe=False)
+setup(
+    name='typeopt',
+    version='0.1',
+    description='Typed docopt',
+    author='Wojciech Stokowiec',
+    author_email='wojciech.stokowiec@tooploox.com',
+    url='https://github.com/1o0ko/typeopt',
+    download_url='https://github.com/1o0ko/typeopt/tarball/0.1',
+    packages=['typeopt'],
+    keywords=['arguments', 'docopt', 'command-line'],
+    install_requires=[
+        'docopt',
+    ],
+    zip_safe=False
+)
